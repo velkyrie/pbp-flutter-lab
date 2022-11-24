0eb8907fbce9319d1447c722ede5e37847f338e0
jinggamentari <vladijm@gmail.com>
2022-11-24 11:46:40 +0700

    minor comment changes

--- counter_7/lib/page/mywatchlist.dart
+++ counter_7/lib/page/mywatchlist.dart
@@ -30,7 +30,7 @@ class _MyWatchlistPageState extends State<MyWatchlistPage> {
     // melakukan decode response menjadi bentuk json
     var data = jsonDecode(utf8.decode(response.bodyBytes));
 
-    // melakukan konversi data json menjadi object ToDo
+    // melakukan konversi data json menjadi object MyWatchlist
     List<MyWatchlist> listMyWatchlist = [];
     for (var d in data) {
       if (d != null) {
